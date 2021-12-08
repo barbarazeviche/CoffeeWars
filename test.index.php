7329b20569986205afff5b2a9cde454a178faced
Iman Chamileva <iman.moussaevna@gmail.com>
2021-12-08 18:41:16 +0100

    1st commit iman branch

--- test.index.php
+++ test.index.php
@@ -5,8 +5,40 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     
+<div id="container">
+        <div id="board">       
+            <div class="wrapper">
+                <div id="bloc1">
+                 
+                    <div id="wc"></div>
+                    <div class="local6"></div>
+                    <div id="wc">WC2</div>
+                </div>
+
+                <div id="bloc2">
+                    <div class="local1">Cafet'</div>
+                    <div class="local6"></div>
+                    <div class="local2">Wad</div>
+                </div>
+
+                <div id="bloc3">
+                    <div class="local3">Profs</div>
+                    <div class="local6"></div>
+                    <div class="local4">Web</div>
+                </div>
+
+                <div id="bloc4">
+                    <div class="local5">ASR</div>
+                    <div class="local6">Entrée</div>
+                    <div class="local7">Game</div>
+                </div>
+            </div>
+        </div>
+    </div>
+
 </body>
 </html>
