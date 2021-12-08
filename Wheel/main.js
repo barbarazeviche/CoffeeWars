7259fecf14793eca8287f2e5763d30a709b543c2
Laure Hurlet <laure.hurlet@gmail.com>
2021-12-08 17:58:18 +0100

    update wheel

--- Wheel/main.js
+++ Wheel/main.js
@@ -5,14 +5,32 @@ let number = Math.ceil(Math.random() * 1000);
 btn.onclick = function() {
     container.style.transform = "rotate(" + number + "deg)";
     number += Math.ceil(Math.random() * 1000);
+    let selectedDiv = []
+
     setTimeout(()=>{
-        container.child.forEach((item)=>{
-            let offset = item;
-            console.log(offset);
+        container.childNodes.forEach((item)=>{
+            if(item.nodeType != 3)
+            {
+                selectedDiv.push(offset(item.childNodes[0]))
+            }
         })
-        console.log(container);
+
+        selectedDiv.sort((a ,b) => {
+            return a.top - b.top
+        })
+
+        
+            console.log(selectedDiv[0])
     },1500);
 }
 
+
+
+function offset(el) {
+    var rect = el.getBoundingClientRect(),
+    scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+    scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    return { top: rect.top + scrollTop, left: rect.left + scrollLeft, name : el.innerHTML, item : el }
+}
 
 
