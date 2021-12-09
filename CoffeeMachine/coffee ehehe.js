945bf91cb7dacaa0d35e23cf92d7e47f22711b7c
Barbara <barbara.branco.c@gmail.com>
2021-12-09 15:44:42 +0100

    addedCoffeeMachine

---
+++ CoffeeMachine/coffee ehehe.js
@@ -0,0 +1,240 @@
+(function (cjs, an) {
+
+var p; // shortcut to reference prototypes
+var lib={};var ss={};var img={};
+lib.ssMetadata = [
+		{name:"coffee ehehe_atlas_1", frames: [[0,0,603,648]]}
+];
+
+
+(lib.AnMovieClip = function(){
+	this.actionFrames = [];
+	this.ignorePause = false;
+	this.gotoAndPlay = function(positionOrLabel){
+		cjs.MovieClip.prototype.gotoAndPlay.call(this,positionOrLabel);
+	}
+	this.play = function(){
+		cjs.MovieClip.prototype.play.call(this);
+	}
+	this.gotoAndStop = function(positionOrLabel){
+		cjs.MovieClip.prototype.gotoAndStop.call(this,positionOrLabel);
+	}
+	this.stop = function(){
+		cjs.MovieClip.prototype.stop.call(this);
+	}
+}).prototype = p = new cjs.MovieClip();
+// symbols:
+
+
+
+(lib.CoffeeMachinelastchangesmachine = function() {
+	this.initialize(ss["coffee ehehe_atlas_1"]);
+	this.gotoAndStop(0);
+}).prototype = p = new cjs.Sprite();
+// helper functions:
+
+function mc_symbol_clone() {
+	var clone = this._cloneProps(new this.constructor(this.mode, this.startPosition, this.loop, this.reversed));
+	clone.gotoAndStop(this.currentFrame);
+	clone.paused = this.paused;
+	clone.framerate = this.framerate;
+	return clone;
+}
+
+function getMCSymbolPrototype(symbol, nominalBounds, frameBounds) {
+	var prototype = cjs.extend(symbol, cjs.MovieClip);
+	prototype.clone = mc_symbol_clone;
+	prototype.nominalBounds = nominalBounds;
+	prototype.frameBounds = frameBounds;
+	return prototype;
+	}
+
+
+(lib.coffemachineGOOD = function(mode,startPosition,loop,reversed) {
+if (loop == null) { loop = true; }
+if (reversed == null) { reversed = false; }
+	var props = new Object();
+	props.mode = mode;
+	props.startPosition = startPosition;
+	props.labels = {};
+	props.loop = loop;
+	props.reversed = reversed;
+	cjs.MovieClip.apply(this,[props]);
+
+	// Layer_1
+	this.instance = new lib.CoffeeMachinelastchangesmachine();
+	this.instance.setTransform(0,0,0.18,0.18);
+
+	this.timeline.addTween(cjs.Tween.get(this.instance).wait(1));
+
+	this._renderFirstFrame();
+
+}).prototype = getMCSymbolPrototype(lib.coffemachineGOOD, new cjs.Rectangle(0,0,108.6,116.7), null);
+
+
+// stage content:
+(lib.coffeeehehe = function(mode,startPosition,loop,reversed) {
+if (loop == null) { loop = true; }
+if (reversed == null) { reversed = false; }
+	var props = new Object();
+	props.mode = mode;
+	props.startPosition = startPosition;
+	props.labels = {};
+	props.loop = loop;
+	props.reversed = reversed;
+	cjs.MovieClip.apply(this,[props]);
+
+	// Layer_2
+	this.instance = new lib.coffemachineGOOD();
+	this.instance.setTransform(156.45,585.8,2.3099,2.3099,0,0,0,54.4,58.6);
+	this.instance._off = true;
+
+	this.timeline.addTween(cjs.Tween.get(this.instance).wait(69).to({_off:false},0).wait(1).to({regX:54.3,regY:58.3,scaleX:2.31,scaleY:2.31,x:169.2,y:576.7},0).wait(1).to({x:182.15,y:568.35},0).wait(1).to({x:195.15,y:560},0).wait(1).to({x:208.1,y:551.65},0).wait(1).to({x:221.1,y:543.3},0).wait(1).to({x:234.05,y:534.95},0).wait(1).to({x:247.05,y:526.6},0).wait(1).to({x:260,y:518.25},0).wait(1).to({x:285.8,y:534.05},0).wait(1).to({x:311.55,y:549.8},0).wait(1).to({x:337.35,y:565.6},0).wait(1).to({x:363.1,y:581.35},0).wait(1).to({x:388.9,y:597.15},0).wait(1).to({x:414.65,y:612.9},0).wait(1).to({x:443.1,y:596.7},0).wait(1).to({x:471.55,y:580.45},0).wait(1).to({x:500,y:564.25},0).wait(1).to({x:528.45,y:548},0).wait(1).to({x:556.9,y:531.8},0).wait(1).to({x:585.3,y:515.55},0).wait(1).to({x:605.1,y:526.9},0).wait(1).to({x:624.85,y:538.25},0).wait(1).to({x:644.65,y:549.55},0).wait(1).to({x:664.4,y:560.9},0).wait(1).to({x:684.2,y:572.25},0).wait(1).to({x:703.95,y:583.55},0).wait(1).to({x:720.65,y:575.05},0).wait(1).to({x:737.3,y:566.55},0).wait(1).to({x:754,y:558.05},0).wait(1).to({x:770.65,y:549.55},0).wait(1).to({x:787.3,y:541.05},0).wait(1).to({x:804,y:532.55},0).wait(1).to({x:820.65,y:524.05},0).wait(1).to({x:837.3,y:515.55},0).wait(1).to({x:846.45,y:524.45},0).wait(1).to({x:855.55,y:533.35},0).wait(1).to({x:864.65,y:542.2},0).wait(1).to({x:873.75,y:551.1},0).wait(1).to({x:882.85,y:560},0).wait(1).to({x:891.95,y:568.85},0).to({_off:true},1).wait(116));
+
+	// Layer_3
+	this.instance_1 = new lib.coffemachineGOOD();
+	this.instance_1.setTransform(130.75,136.25,2.0599,2.0599,0,0,180,54.2,58.6);
+	this.instance_1._off = true;
+
+	this.timeline.addTween(cjs.Tween.get(this.instance_1).wait(179).to({_off:false},0).wait(1).to({regX:54.3,regY:58.3,scaleX:1.9702,scaleY:2.0582,x:150.75,y:123.2},0).wait(1).to({scaleX:1.8804,scaleY:2.0564,x:171,y:110.8},0).wait(1).to({scaleX:1.7906,scaleY:2.0546,x:191.2,y:98.35},0).wait(1).to({scaleX:1.7008,scaleY:2.0528,x:211.45,y:85.9},0).wait(1).to({scaleX:1.6111,scaleY:2.0511,x:231.65,y:73.5},0).wait(1).to({scaleX:1.5213,scaleY:2.0493,x:251.9,y:61},0).wait(1).to({scaleX:1.4315,scaleY:2.0475,x:270.1,y:69.65},0).wait(1).to({scaleX:1.3417,scaleY:2.0457,x:288.35,y:78.35},0).wait(1).to({scaleX:1.252,scaleY:2.044,x:306.55,y:87},0).wait(1).to({scaleX:1.1622,scaleY:2.0422,x:324.8,y:95.7},0).wait(1).to({scaleX:1.0724,scaleY:2.0404,x:343.05,y:104.35},0).wait(1).to({scaleX:0.9826,scaleY:2.0386,x:361.3,y:113},0).wait(1).to({scaleX:0.8929,scaleY:2.0368,x:381.95,y:99.7},0).wait(1).to({scaleX:0.8031,scaleY:2.0351,x:402.6,y:86.35},0).wait(1).to({scaleX:0.7133,scaleY:2.0333,x:423.25,y:73.05},0).wait(1).to({scaleX:0.6235,scaleY:2.0315,x:443.95,y:59.7},0).wait(1).to({scaleX:0.5338,scaleY:2.0297,x:464.6,y:46.4},0).wait(1).to({scaleX:0.444,scaleY:2.028,x:485.3,y:33.05},0).wait(1).to({scaleX:0.3542,scaleY:2.0262,x:501.65,y:42.55},0).wait(1).to({scaleX:0.2644,scaleY:2.0244,x:518.1,y:52.05},0).wait(1).to({scaleX:0.1747,scaleY:2.0226,x:534.45,y:61.55},0).wait(1).to({scaleX:0.0849,scaleY:2.0209,x:550.85,y:71.1},0).wait(1).to({scaleX:0.0049,scaleY:2.0191,skewY:0,x:567.2,y:80.6},0).wait(1).to({scaleX:0.0947,scaleY:2.0173,x:583.6,y:90.15},0).wait(1).to({scaleX:0.1844,scaleY:2.0155,x:599.95,y:99.7},0).wait(1).to({scaleX:0.2742,scaleY:2.0138,x:625.1,y:85.7},0).wait(1).to({scaleX:0.364,scaleY:2.012,x:650.2,y:71.7},0).wait(1).to({scaleX:0.4538,scaleY:2.0102,x:675.35,y:57.7},0).wait(1).to({scaleX:0.5435,scaleY:2.0084,x:700.4,y:43.7},0).wait(1).to({scaleX:0.6333,scaleY:2.0066,x:725.55,y:29.7},0).wait(1).to({scaleX:0.7231,scaleY:2.0049,x:750.65,y:15.7},0).wait(1).to({scaleX:0.8129,scaleY:2.0031,x:767.8,y:26.2},0).wait(1).to({scaleX:0.9026,scaleY:2.0013,x:784.95,y:36.65},0).wait(1).to({scaleX:0.9924,scaleY:1.9995,x:802.1,y:47.1},0).wait(1).to({scaleX:1.0822,scaleY:1.9978,x:819.25,y:57.6},0).wait(1).to({scaleX:1.172,scaleY:1.996,x:836.4,y:68.05},0).wait(1).to({scaleX:1.2617,scaleY:1.9942,x:853.55,y:78.55},0).wait(1).to({scaleX:1.3515,scaleY:1.9924,x:870.7,y:89.05},0).wait(1).to({scaleX:1.4413,scaleY:1.9907,x:875.85,y:96.65},0).wait(1).to({scaleX:1.5311,scaleY:1.9889,x:881,y:104.25},0).wait(1).to({scaleX:1.6208,scaleY:1.9871,x:886.15,y:111.9},0).wait(1).to({scaleX:1.7106,scaleY:1.9853,x:891.3,y:119.45},0).wait(1).to({scaleX:1.8004,scaleY:1.9836,x:896.45,y:127.05},0).wait(1).to({scaleX:1.8902,scaleY:1.9818,x:901.65,y:134.7},0).wait(1).to({scaleX:1.9799,scaleY:1.98,x:906.75,y:142.3},0).to({_off:true},1).wait(1));
+
+	// Layer_1
+	this.instance_2 = new lib.coffemachineGOOD();
+	this.instance_2.setTransform(900.05,143.1,1.94,1.94,0,0,0,54.3,58.4);
+
+	this.timeline.addTween(cjs.Tween.get(this.instance_2).to({scaleX:1.45,scaleY:1.45,x:546.75,y:327},35).to({scaleX:2.2899,scaleY:2.2899,x:157.45,y:586.95},34).to({_off:true},1).wait(40).to({_off:false,skewY:180,x:893.2,y:568.15},0).to({scaleX:1.54,scaleY:1.54,x:517.25,y:364.15},34).to({regX:54.2,regY:58.3,scaleX:2.092,scaleY:2.0919,x:131.85,y:136.15},35).to({_off:true},1).wait(45).to({_off:false,regX:54.3,regY:58.4,scaleX:1.94,scaleY:1.94,skewY:0,x:900.05,y:143.1},0).wait(1));
+
+	this._renderFirstFrame();
+
+}).prototype = p = new lib.AnMovieClip();
+p.nominalBounds = new cjs.Rectangle(530.2,282.8,487.4,464.90000000000003);
+// library properties:
+lib.properties = {
+	id: '855A96F920306440AA12076661BFFBF6',
+	width: 1024,
+	height: 768,
+	fps: 24,
+	color: "#FFFFFF",
+	opacity: 1.00,
+	manifest: [
+		{src:"images/coffee ehehe_atlas_1.png", id:"coffee ehehe_atlas_1"}
+	],
+	preloads: []
+};
+
+
+
+// bootstrap callback support:
+
+(lib.Stage = function(canvas) {
+	createjs.Stage.call(this, canvas);
+}).prototype = p = new createjs.Stage();
+
+p.setAutoPlay = function(autoPlay) {
+	this.tickEnabled = autoPlay;
+}
+p.play = function() { this.tickEnabled = true; this.getChildAt(0).gotoAndPlay(this.getTimelinePosition()) }
+p.stop = function(ms) { if(ms) this.seek(ms); this.tickEnabled = false; }
+p.seek = function(ms) { this.tickEnabled = true; this.getChildAt(0).gotoAndStop(lib.properties.fps * ms / 1000); }
+p.getDuration = function() { return this.getChildAt(0).totalFrames / lib.properties.fps * 1000; }
+
+p.getTimelinePosition = function() { return this.getChildAt(0).currentFrame / lib.properties.fps * 1000; }
+
+an.bootcompsLoaded = an.bootcompsLoaded || [];
+if(!an.bootstrapListeners) {
+	an.bootstrapListeners=[];
+}
+
+an.bootstrapCallback=function(fnCallback) {
+	an.bootstrapListeners.push(fnCallback);
+	if(an.bootcompsLoaded.length > 0) {
+		for(var i=0; i<an.bootcompsLoaded.length; ++i) {
+			fnCallback(an.bootcompsLoaded[i]);
+		}
+	}
+};
+
+an.compositions = an.compositions || {};
+an.compositions['855A96F920306440AA12076661BFFBF6'] = {
+	getStage: function() { return exportRoot.stage; },
+	getLibrary: function() { return lib; },
+	getSpriteSheet: function() { return ss; },
+	getImages: function() { return img; }
+};
+
+an.compositionLoaded = function(id) {
+	an.bootcompsLoaded.push(id);
+	for(var j=0; j<an.bootstrapListeners.length; j++) {
+		an.bootstrapListeners[j](id);
+	}
+}
+
+an.getComposition = function(id) {
+	return an.compositions[id];
+}
+
+
+an.makeResponsive = function(isResp, respDim, isScale, scaleType, domContainers) {		
+	var lastW, lastH, lastS=1;		
+	window.addEventListener('resize', resizeCanvas);		
+	resizeCanvas();		
+	function resizeCanvas() {			
+		var w = lib.properties.width, h = lib.properties.height;			
+		var iw = window.innerWidth, ih=window.innerHeight;			
+		var pRatio = window.devicePixelRatio || 1, xRatio=iw/w, yRatio=ih/h, sRatio=1;			
+		if(isResp) {                
+			if((respDim=='width'&&lastW==iw) || (respDim=='height'&&lastH==ih)) {                    
+				sRatio = lastS;                
+			}				
+			else if(!isScale) {					
+				if(iw<w || ih<h)						
+					sRatio = Math.min(xRatio, yRatio);				
+			}				
+			else if(scaleType==1) {					
+				sRatio = Math.min(xRatio, yRatio);				
+			}				
+			else if(scaleType==2) {					
+				sRatio = Math.max(xRatio, yRatio);				
+			}			
+		}
+		domContainers[0].width = w * pRatio * sRatio;			
+		domContainers[0].height = h * pRatio * sRatio;
+		domContainers.forEach(function(container) {				
+			container.style.width = w * sRatio + 'px';				
+			container.style.height = h * sRatio + 'px';			
+		});
+		stage.scaleX = pRatio*sRatio;			
+		stage.scaleY = pRatio*sRatio;
+		lastW = iw; lastH = ih; lastS = sRatio;            
+		stage.tickOnUpdate = false;            
+		stage.update();            
+		stage.tickOnUpdate = true;		
+	}
+}
+an.handleSoundStreamOnTick = function(event) {
+	if(!event.paused){
+		var stageChild = stage.getChildAt(0);
+		if(!stageChild.paused || stageChild.ignorePause){
+			stageChild.syncStreamSounds();
+		}
+	}
+}
+an.handleFilterCache = function(event) {
+	if(!event.paused){
+		var target = event.target;
+		if(target){
+			if(target.filterCacheList){
+				for(var index = 0; index < target.filterCacheList.length ; index++){
+					var cacheInst = target.filterCacheList[index];
+					if((cacheInst.startFrame <= target.currentFrame) && (target.currentFrame <= cacheInst.endFrame)){
+						cacheInst.instance.cache(cacheInst.x, cacheInst.y, cacheInst.w, cacheInst.h);
+					}
+				}
+			}
+		}
+	}
+}
+
+
+})(createjs = createjs||{}, AdobeAn = AdobeAn||{});
+var createjs, AdobeAn;
